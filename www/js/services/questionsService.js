a0b382dc8732c431ad0216ca53ee4717cfaa2eef
a-pitan <jtqnr6500@yahoo.co.jp>
2016-06-05 00:20:06 +0900

    変えたような。

---
+++ www/js/services/questionsService.js
@@ -0,0 +1,34 @@
+app.value('questionsService', {
+    questions : [
+	　　/*{
+	        "text": "問題文",
+	        "answer": "答え",
+	        "choices": ["答え以外の選択肢","答え以外の選択肢","答え以外の選択肢"]
+	    },*/
+        {
+	    	"text": "Google社製のWEBブラウザは？",
+	    	"answer": "Chrome",
+	    	"choices": ["FireFox","Opera","Safari"]
+	    },
+        {
+        	"text": "嫌な仕事は？",
+	    	"answer": "電話",
+	    	"choices": ["掃除","パシリ","パソコン"]
+	    },
+        {
+            "text": "ロックマンの発売日は？",
+	    	"answer": "1990",
+	    	"choices": ["昨日","夢","あ～～～"]
+	    },
+        {
+            "text": "良さげな果物",
+        	"answer": "ライチ",
+	    	"choices": ["バナナ","山ぶどう","味"]
+	    },
+        {
+            "text": "スペル",
+        	"answer": "WordPress",
+	    	"choices": ["Wordpress","wordpress","ワ～ドプレ～ス"]
+        },
+	]
+});
